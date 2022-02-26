f6852fad5bc586ed7988ef05073431ddd2c96a8f
akhmadinurbol <akh9thz@gmail.com>
2022-02-26 20:55:35 +0600

    Code correcting

--- src/java/com/intexsoft/repositories/interfaces/IOutputRepository.java
+++
@@ -1,9 +0,0 @@
-package com.intexsoft.repositories.interfaces;
-
-import com.intexsoft.models.Book;
-
-import java.util.List;
-
-public interface IOutputRepository {
-    void outputAll(List<Book> books);
-}
